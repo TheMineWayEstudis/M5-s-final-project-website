f4d5fc0602226dc83737b817c7f2fafd7d9435cf
Joel Campos <joelcamposoliva9000@gmail.com>
2021-12-28 16:06:19 +0100

    Main screen with images

--- web-m5/src/view/mainScreen/MainScreen.tsx
+++ web-m5/src/view/mainScreen/MainScreen.tsx
@@ -1,9 +1,12 @@
 import MainScreenBodyItems from './MainScreenBodyItems';
+import MainScreenCarousel from './MainScreenCarousel';
 import MainScreenHeader from './MainScreenHeader';
 
 export default function MainScreen() {
     return (
         <>
+            <MainScreenCarousel/>
+            <br/>
             <MainScreenHeader/>
             <br/>
             <MainScreenBodyItems/>
--- web-m5/src/view/mainScreen/MainScreenBodyItems.tsx
+++ web-m5/src/view/mainScreen/MainScreenBodyItems.tsx
@@ -1,12 +1,13 @@
 import { Card, Col, Row } from 'antd';
-import React from 'react'
 
 export default function MainScreenBodyItems() {
     return (
         <div className="site-card-wrapper">
             <Row gutter={[24, 24]}>
-                <Col lg={8} style={{display: 'flex'}}>
-                    <Card>
+                <Col sm={8} lg={6} style={{display: 'flex'}}>
+                    <Card
+                        cover={<img src={require('./resources/react.jpg')}/>}
+                    >
                         <h2 style={{ fontWeight: 'bold' }}>About the site</h2>
                         <p style={{ textAlign: 'justify' }}>
                             In order to create this website I decided to use React, a JavaScript library used to create modern and well performant websites easily. To ease the development the source code is written in TypeScript instead of JavaScript.
@@ -15,8 +16,10 @@ export default function MainScreenBodyItems() {
                         </p>
                     </Card>
                 </Col>
-                <Col lg={8} style={{display: 'flex'}}>
-                    <Card>
+                <Col sm={8} lg={6} style={{display: 'flex'}}>
+                    <Card
+                        cover={<img src={require('./resources/github.jpg')}/>}
+                    >
                         <h2 style={{ fontWeight: 'bold' }}>Project source code</h2>
                         <p style={{ textAlign: 'justify' }}>
                             As every good developer I have the source code of this project on a public GitHub repository so everybody is able to see how the website works underthehood.
