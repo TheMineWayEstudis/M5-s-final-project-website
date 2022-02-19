0682d94b86a3c969001b227e3843c7e52601b5e8
Joel Campos <joelcamposoliva9000@gmail.com>
2022-02-19 15:40:47 +0100

    Choosen badge

--- web-m5/src/view/marketStudy/MarketStudyElement.tsx
+++ web-m5/src/view/marketStudy/MarketStudyElement.tsx
@@ -1,10 +1,10 @@
-import { Card, Image } from "antd"
+import { Badge, Card, Image } from "antd"
 
 export type MarketStudyItem = {
     imageUrl: string;
     name: string;
     purchaseUrl: string;
-    
+
     choosen?: boolean;
 }
 
@@ -15,13 +15,30 @@ type Props = {
 export default function MarketStudyElement(props: Props) {
     const item = props.item;
 
+    const Choosen = (props: { children: JSX.Element }) => {
+        if (item.choosen) {
+            return (
+                <Badge.Ribbon
+                    text="Choosen"
+                    color="green"
+                >
+                    {props.children}
+                </Badge.Ribbon>
+            )
+        }
+
+        return props.children;
+    }
+
     return (
-        <Card
-            hoverable
-            cover={<Image style={{width: '100%'}} src={item.imageUrl}/>}
-            title={item.name}
-        >
-            
-        </Card>
+        <Choosen>
+            <Card
+                hoverable
+                cover={<Image style={{ width: '100%' }} src={item.imageUrl} />}
+                title={item.name}
+            >
+
+            </Card>
+        </Choosen>
     );
 }
