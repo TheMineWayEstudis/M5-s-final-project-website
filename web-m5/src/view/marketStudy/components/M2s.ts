2f382f05b9df9d2a95c731232623e3cb6b88c81f
Joel Campos <joelcamposoliva9000@gmail.com>
2022-03-02 21:58:44 +0100

    Choosen another m2

--- web-m5/src/view/marketStudy/components/M2s.ts
+++ web-m5/src/view/marketStudy/components/M2s.ts
@@ -2,12 +2,26 @@ import { MarketStudyItem } from "../MarketStudyElement";
 
 const m2s: MarketStudyItem[] = [
     {
+        name: "Team Group CARDEA A440",
+        price: 340.12,
+        imageUrl: "https://img.pccomponentes.com/articles/58/589396/1271-team-group-cardea-a440-2tb-gaming-ssd-pcie-gen4-x4-nvme-14.jpg",
+        purchaseUrl: "https://www.pccomponentes.com/team-group-cardea-a440-2tb-gaming-ssd-pcie-gen4-x4-nvme-14",
+        manufacturer: "Team Group",
+        choosen: true,
+        specs: {
+            diskSpace: 2,
+            diskRead: 7000,
+            diskWrite: 7000,
+            watercooling: false,
+            hardwareEncryption: false,
+        }
+    },
+    {
         name: "Corsair MP600 PRO Gen 4",
         price: 341.31,
         imageUrl: "https://img.pccomponentes.com/articles/35/357145/1564-corsair-mp600-pro-2tb-m2-nvme-pcie-gen-4-x4-hydro-x-ed-ssd-4eba8a55-1735-469b-a5f8-48ca595559ab.jpg",
         purchaseUrl: "https://www.pccomponentes.com/corsair-mp600-pro-2tb-m2-nvme-pcie-gen-4-x4-hydro-x-ed-ssd",
         manufacturer: "Corsair",
-        choosen: true,
         specs: {
             diskSpace: 2,
             diskRead: 7000,
@@ -16,20 +30,6 @@ const m2s: MarketStudyItem[] = [
             watercooling: true,
             hardwareEncryption: true,
         },
-    },
-    {
-        name: "Team Group CARDEA A440",
-        price: 340.12,
-        imageUrl: "https://img.pccomponentes.com/articles/58/589396/1271-team-group-cardea-a440-2tb-gaming-ssd-pcie-gen4-x4-nvme-14.jpg",
-        purchaseUrl: "https://www.pccomponentes.com/team-group-cardea-a440-2tb-gaming-ssd-pcie-gen4-x4-nvme-14",
-        manufacturer: "Team Group",
-        specs: {
-            diskSpace: 2,
-            diskRead: 7000,
-            diskWrite: 7000,
-            watercooling: false,
-            hardwareEncryption: false,
-        }
     },
     {
         name: "MSI Spatium M480 Gen 4",
