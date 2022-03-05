3dae57772c75fda20f9294ccdb2c2786eb2b8e37
Joel Campos <joelcamposoliva9000@gmail.com>
2022-03-05 22:49:13 +0100

    Deleted old elements

--- web-m5/src/view/mainScreen/MainScreen.tsx
+++ web-m5/src/view/mainScreen/MainScreen.tsx
@@ -1,13 +1,7 @@
-import MainScreenBodyItems from './MainScreenBodyItems';
-import MainScreenCarousel from './MainScreenCarousel';
-import MainScreenHeader from './MainScreenHeader';
-
 export default function MainScreen() {
     return (
         <>
-            <MainScreenHeader/>
-            <br/>
-            <MainScreenBodyItems/>
+            
         </>
     );
 }
