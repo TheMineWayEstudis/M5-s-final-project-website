a0e4287d36370c7f986a7676f8d0c53b72a440d3
Joel Campos <joelcamposoliva9000@gmail.com>
2022-03-06 12:12:02 +0100

    CPU installation

--- web-m5/src/view/howToBuildIt/steps/motherboard.tsx
+++ web-m5/src/view/howToBuildIt/steps/motherboard.tsx
@@ -35,6 +35,29 @@ const motherboard: StepItem = {
                     <Alert showIcon type="warning" description={'You must check that all RAM is installed proporly. If any is partially out of the slot you can break the RAM, the slot or both.'}/>
                 </IP>
             </>
+            <>
+                <SCTitle>CPU installation</SCTitle>
+                <IP
+                    images={[
+                        {
+                            url: require('./resources/motherboard/CPU_install_1.PNG'),
+                            reference: "CPU install",
+                        },
+                        {
+                            url: require('./resources/motherboard/CPU_install_2.PNG'),
+                            reference: "CPU install (bar)",
+                        }
+                    ]}
+                >
+                    <p>
+                        We unbox the CPU. Then we open the CPU socket (there is a gray bar that allows us to do so) and look for a golden triangle on the bac of the CPU. This golden triangle shows us how the CPU needs to be placed (the triangle must match the triangle placed on the motherboard).
+                        Once we know the position, we put the CPU on the socket.
+                    </p>
+                    <p>
+                        Finally, we push down the gray bar again (this time it might be harder, but don't worry, it won't break).
+                    </p>
+                </IP>
+            </>
         </>
     ),
 };
