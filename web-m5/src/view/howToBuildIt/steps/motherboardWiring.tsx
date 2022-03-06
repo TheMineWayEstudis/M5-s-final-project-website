75012c2a17c0f562c46e798d01a424cfb6c7383b
Joel Campos <joelcamposoliva9000@gmail.com>
2022-03-06 18:26:09 +0100

    Message

--- web-m5/src/view/howToBuildIt/steps/motherboardWiring.tsx
+++ web-m5/src/view/howToBuildIt/steps/motherboardWiring.tsx
@@ -1,11 +1,21 @@
 import { StepItem } from "../HowToBuildItSteps";
+import SCTitle from "./components/StepCheckableTitle";
+import P from "./components/StepParagraph";
+import IP from "./components/StepParagrapthWithImages";
 
 const motherboardWiring: StepItem = {
     name: "Motherboard wiring",
     description: "We connect as much wires as we are able to.",
     component: (
         <>
-            
+            <>
+                <SCTitle>Motherboard wiring</SCTitle>
+                <IP>
+                    <p>
+                        We try to connect as much wires as we can to the mothermoard.
+                    </p>
+                </IP>
+            </>
         </>
     ),
 }
