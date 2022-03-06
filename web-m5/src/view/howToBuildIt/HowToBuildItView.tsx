3874936045748b940a18b3dc53929ea883656184
Joel Campos <joelcamposoliva9000@gmail.com>
2022-03-07 00:40:24 +0100

    Installation end

--- web-m5/src/view/howToBuildIt/HowToBuildItView.tsx
+++ web-m5/src/view/howToBuildIt/HowToBuildItView.tsx
@@ -1,4 +1,4 @@
-import { LeftOutlined, RightOutlined } from '@ant-design/icons';
+import { FlagOutlined, LeftOutlined, RightOutlined } from '@ant-design/icons';
 import { Button, Card, Col, Row, Space } from 'antd';
 import { useState } from 'react';
 import HowToBuildItSteps, { StepItem } from './HowToBuildItSteps';
@@ -12,6 +12,8 @@ import graphics from './steps/graphics';
 import checks from './steps/checks';
 import additional from './steps/addtional';
 import startup from './steps/startup';
+import HowToBuildItEnd from './HowToBuildItEnd';
+import Confetti from 'react-confetti';
 
 const steps: StepItem[] = [
     setup,
@@ -29,35 +31,42 @@ export default function HowToBuildItView() {
     const [step, setStep] = useState<number>(0);
 
     return (
-        <Row
-            gutter={[24, 24]}
-            justify='center'
-        >
-            <Col span={24}>
-                <HowToBuildItSteps
-                    step={step}
-                    steps={steps}
-                    setStep={setStep}
-                />
-            </Col>
-            <Col
-                xs={24}
-                md={21}
+        <>
+            {step >= steps.length && <Confetti/>}
+            <Row
+                gutter={[24, 24]}
+                justify='center'
             >
-                <Row gutter={[24, 24]}>
-                    <Col span={24}>
-                        <Card>
-                            <Space>
-                                <Button icon={<LeftOutlined />} disabled={step <= 0} onClick={() => setStep(step - 1)}>Back</Button>
-                                <Button icon={<RightOutlined />} disabled={step >= steps.length - 1} onClick={() => setStep(step + 1)}>Next</Button>
-                            </Space>
-                        </Card>
-                    </Col>
-                    <Col span={24}>
-                        {steps[step].component}
-                    </Col>
-                </Row>
-            </Col>
-        </Row>
+                <Col span={24}>
+                    <HowToBuildItSteps
+                        step={step}
+                        steps={steps}
+                        setStep={setStep}
+                    />
+                </Col>
+                <Col
+                    xs={24}
+                    md={21}
+                >
+                    <Row gutter={[24, 24]}>
+                        <Col span={24}>
+                            <Card>
+                                <Space>
+                                    <Button icon={<LeftOutlined />} disabled={step <= 0} onClick={() => setStep(step - 1)}>Back</Button>
+                                    <Button icon={step >= steps.length - 1 ? <FlagOutlined /> : <RightOutlined />} disabled={step >= steps.length} onClick={() => setStep(step + 1)}>{step >= steps.length - 1 ? "End installation" : "Next"}</Button>
+                                </Space>
+                            </Card>
+                        </Col>
+                        <Col span={24}>
+                            {
+                                step >= steps.length ? (
+                                    <HowToBuildItEnd />
+                                ) : (<>{steps[step].component}</>)
+                            }
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+        </>
     );
 }
